3ac2b545b3aa0a67ef7b3053e76f0d829764899a
Med Oussama Braiek <73386969+MedOussamaBraiek@users.noreply.github.com>
2023-01-07 17:21:47 +0100

    Fron Url

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
 const patientRouter = require('./routers/patientRouters');
 const userRouter = require('./routers/userRouters');
 const cors = require("cors")
@@ -14,26 +14,26 @@ app.use(require('body-parser').json());
 // mongoose to connect and interact with DataBase
 const dbURI = 'mongodb+srv://karim:789456123@dentistdb.jucqsfg.mongodb.net/DentistDB?retryWrites=true&w=majority'
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => { 
-        app.listen(5000); 
-        console.log('connected to db') 
-    })
-    .catch((err) => console.log(err)); 
+  .then((result) => {
+    app.listen(5000);
+    console.log('connected to db')
+  })
+  .catch((err) => console.log(err));
 
-    
-const whitelist = ["https://dentiste-karim.vercel.app"]
+
+const whitelist = ["https://docteur-karim-somai.vercel.app"]
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || whitelist.indexOf(origin) !== -1) {
-          callback(null, true)
-        } else {
-          callback(new Error("Not allowed by CORS"))
-        }
-      },
-      credentials: true,
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
     }
+  },
+  credentials: true,
+}
 app.use(cors(corsOptions))
 
 
-app.use('/patients',patientRouter);
-app.use('/user',userRouter);
+app.use('/patients', patientRouter);
+app.use('/user', userRouter);
